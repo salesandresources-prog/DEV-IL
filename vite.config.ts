876e4353b5c28a922546479477697956e3f65898
Andres Aguiar <socialgroupgerencia@gmail.com>
2026-08-08 18:19:05 -0500

    Limpieza de archivos viejos y fix del build en Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  nitro: {
-    preset: 'vercel',
+  tanstackStart: {
+    server: {
+      preset: 'vercel'
+    }
   },
+  nitro: false,
   server: {
     host: '0.0.0.0', // Esto es lo que permite la conexión externa
     port: 5173,
