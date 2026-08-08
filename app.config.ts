f615bee8f5ddaf6baa3daf1f784bf0363de5a558
Andres Aguiar <socialgroupgerencia@gmail.com>
2026-08-08 18:04:32 -0500

    Corregido build usando Vite nativo

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,1 @@
-import { defineConfig } from '@tanstack/react-start/config'
-import tsConfigPaths from 'vite-tsconfig-paths'
-
-export default defineConfig({
-  server: {
-    preset: 'vercel',
-  },
-  vite: {
-    plugins: [
-      tsConfigPaths({
-        projects: ['./tsconfig.json'],
-      }),
-    ],
-  },
-})
+// Deleted
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  nitro: {
+    preset: 'vercel',
+  },
   server: {
     host: '0.0.0.0', // Esto es lo que permite la conexión externa
     port: 5173,
